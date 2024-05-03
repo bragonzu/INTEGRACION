918f27564779cd55d6c29292e5052433252ee674
bragonzu <bragonzu@hotmail.com>
2024-05-03 02:10:19 -0500

    [ADD] Se agrega cambios respectivos

--- WEBPY/inventario/admin.py
+++ WEBPY/inventario/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here for see in administrator.
-from .models import Usuario,UnidadOrganica,Sede,Estado,Rol
+from .models import Usuario,UnidadOrganica,Sede,Estado,Rol,Bien
 admin.site.register(Usuario)
 admin.site.register(UnidadOrganica)
 admin.site.register(Sede)
 admin.site.register(Estado)
 admin.site.register(Rol)
+admin.site.register(Bien)
--- WEBPY/inventario/forms.py
+++ WEBPY/inventario/forms.py
@@ -2,19 +2,23 @@ from django import forms
 from .models import Usuario
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate
+from .models import UnidadOrganica,Sede,Estado,Rol
+
 
 class RegistroForm(UserCreationForm):
     nombres = forms.CharField(label='Nombres')
     apellidos = forms.CharField(label='Apellidos')
     email = forms.EmailField(max_length=255)
     dni = forms.CharField(max_length=88)
-        # unidadOrganica = forms.CharField(max_length=10)
-        # sede = forms.CharField(max_length=10)
-        # estado = forms.CharField(max_length=10)
-        # rol = forms.CharField(max_length=10)
+    unidadOrganica = forms.ModelChoiceField(queryset=UnidadOrganica.objects.all(), required=False)
+    sede = forms.ModelChoiceField(queryset=Sede.objects.all(), required=False)
+    estado = forms.ModelChoiceField(queryset=Estado.objects.all(), required=False)
+    rol = forms.ModelChoiceField(queryset=Rol.objects.all(), required=False)
+    
+    
     class Meta:
-        model = Usuario  # Use your custom user model if applicable
-        fields = ['nombres','apellidos','username','email']  # Customize fields
+        model = Usuario 
+        fields = ['nombres','apellidos','dni','unidadOrganica','sede','estado','rol','username']  # Customize fields
 
     def __init__(self, *args, **kwargs):
         super(RegistroForm, self).__init__(*args, **kwargs)
@@ -23,6 +27,20 @@ class RegistroForm(UserCreationForm):
                 field.widget.attrs['class'] += ' form-control'
             else:
                 field.widget.attrs['class']='form-control'
+    
+    def save(self, commit=True):
+        user = super(RegistroForm, self).save(commit=False)
+        user.nombres = self.cleaned_data['nombres']
+        user.apellidos = self.cleaned_data['apellidos']
+        user.email = self.cleaned_data['email']
+        user.dni = self.cleaned_data['dni']
+        user.unidadOrganica = self.cleaned_data['unidadOrganica']
+        user.sede = self.cleaned_data['sede']
+        user.estado = self.cleaned_data['estado']
+        user.rol = self.cleaned_data['rol']
+        if commit:
+            user.save()
+        return user
 
 
 class LoginForm(forms.Form):
@@ -43,61 +61,3 @@ class LoginForm(forms.Form):
         
         return cleaned_data
 
-
-# class RegisterForm(UserCreationForm):
-#     email = forms.EmailField( label='Email')
-#     username = forms.CharField(label='username', min_length=5, max_length=150)  
-#     age = forms.IntegerField(label='Edad',min_value=0)
-#     phone = forms.IntegerField(label='Telefono')
-#     address = forms.CharField(label='Dirección')
-#     image = forms.ImageField(label='Foto')
-    
-#     class Meta:
-#         model = User
-#         fields = ['username','email','password1','age','phone','address','image']
-     
-#     def __init__(self, *args, **kwargs):
-#         super(RegisterForm, self).__init__(*args, **kwargs)
-#         for field_name, field in self.fields.items():
-#             if field.widget.attrs.get('class'):
-#                 field.widget.attrs['class'] += ' form-control'
-#             else:
-#                 field.widget.attrs['class']='form-control'
-
-#     def save(self, commit = True):  
-#         user = User.objects.create_user(  
-#             self.cleaned_data['username'],  
-#             self.cleaned_data['email'],  
-#             self.cleaned_data['password1']  
-#         )  
-#         return user 
-
-
-
-
-# class CreateNewProduct(forms.Form):
-#     SELECTION_TYPE = (
-#         ('L', 'LITROS'),
-#         ('M', 'METROS'),
-#         ('V', 'VOLUMEN'),
-#         ('U', 'UNIDAD'),
-#     )
-
-#     code = forms.CharField(label = " Codigo " , max_length = 10 )
-#     name = forms.CharField(label="Nombre")
-#     description = forms.CharField(label = " Descripcion de tarea ", widget=forms.Textarea)
-#     price_unit = forms.FloatField() 
-#     quantity = forms.FloatField()
-#     type_product = forms.ChoiceField(choices=SELECTION_TYPE)
-#     category = forms.ModelMultipleChoiceField(
-#         queryset= CaAtegory.objects.all(), widget=forms.CheckboxSelectMultiple
-#     )
-#     def __init__(self, *args, **kwargs):
-#         super(CreateNewProduct, self).__init__(*args, **kwargs)
-#         for field_name, field in self.fields.items():
-#             if field.widget.attrs.get('class'):
-#                 field.widget.attrs['class'] += ' form-control'
-#             else:
-#                 field.widget.attrs['class']='form-control'
-#     def save(self):
-#         cleaned_data = self.cleaned_data
